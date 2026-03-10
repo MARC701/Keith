d240e2abe702764a6937376c25ded6d1f0c2bf87
𝘼𝙧𝙨𝙡𝙖𝙣 𝘾𝙝𝙖𝙪𝙙𝙖𝙧𝙮 <143324285+MARC701@users.noreply.github.com>
2026-03-10 14:14:06 +0500

    Update owner number and bot configuration settings

--- settings.js
+++ settings.js
@@ -7,23 +7,23 @@ if (fs.existsSync('set.env')) {
 }
 
 const session = process.env.SESSION || '';
-const dev = process.env.OWNER_NUMBER || '254748387615';
+const dev = process.env.OWNER_NUMBER || '923111977378';
 
 const autostatusAutoviewStatus = process.env.AUTOVIEW_STATUS || 'true';
-const autostatusAutoLikeStatus = process.env.AUTOLIKE_STATUS || 'false';
-const autostatusAutoReplyStatus = process.env.AUTOREPLY_STATUS || 'false';
-const autostatusStatusReplyText = process.env.STATUS_REPLY_TEXT || '✅ Status Viewed By Keith Md';
+const autostatusAutoLikeStatus = process.env.AUTOLIKE_STATUS || 'true';
+const autostatusAutoReplyStatus = process.env.AUTOREPLY_STATUS || 'true';
+const autostatusStatusReplyText = process.env.STATUS_REPLY_TEXT || '✅ Status Viewed By 𝐀𝐫𝐬𝐥𝐚𝐧 𝐂𝐡𝐚𝐮𝐝𝐡𝐚𝐫𝐲 👑';
 const autostatusStatusLikeEmojis = process.env.STATUS_LIKE_EMOJIS || '💛,❤️,💜,🤍,💙';
 
-const botPrefix = process.env.PREFIX || ".";
-const botAuthor = process.env.AUTHOR || "Keith";
-const botUrl = process.env.BOT_PIC || "https://files.catbox.moe/9zqj7g.jpg";
-const botGurl = process.env.BOT_GURL || "https://github.com/Keithkeizzah/KEITH-MD";
-const botTimezone = process.env.BOT_TIMEZONE || "Africa/Nairobi";
-const botBotname = process.env.BOTNAME || "KEITH-MD";
-const botPackname = process.env.BOT_PACKNAME || "KEITH-MD";
+const botPrefix = process.env.PREFIX || "-";
+const botAuthor = process.env.AUTHOR || "𝐀𝐫𝐬𝐥𝐚𝐧 𝐂𝐡𝐚𝐮𝐝𝐡𝐚𝐫𝐲 👑";
+const botUrl = process.env.BOT_PIC || "https://files.catbox.moe/qyogy8.jpg";
+const botGurl = process.env.BOT_GURL || "https://whatsapp.com/channel/0029Vat4TFC0QeaoLURbP61u";
+const botTimezone = process.env.BOT_TIMEZONE || "Asia/Karachi";
+const botBotname = process.env.BOTNAME || "ᴍᴀʀᴄ-ᴍᴅ";
+const botPackname = process.env.BOT_PACKNAME || "ᴍᴀʀᴄ-ᴍᴅ";
 const botMode = process.env.BOT_MODE || "public";
-const botSessionName = process.env.BOT_SESSION_NAME || "keith-md";
+const botSessionName = process.env.BOT_SESSION_NAME || "𝐌𝐀𝐑𝐂-𝐌𝐃";
 
 const { Sequelize } = require('sequelize'); 
 const DATABASE_URL = process.env.DATABASE_URL || './database.db'; 
